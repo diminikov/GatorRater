aba4ac5b9d251380068782d144fc695208d7ba7e
Yonas Worku <92879873+YonasWorku2224@users.noreply.github.com>
2023-02-28 17:21:44 -0500

    Search bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
+
+interface someClass {
+  name: string;
+  professor: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'GatorRater';
+
+  searchTerm = '';
+  classes: someClass[] = [];
+  term = '';
+  allClasses: someClass[] = [];
+
+  constructor(private http: HttpClient) { }
+  
+  ngOnInit(): void {
+    this.http.get<someClass[]>('./assets/data/classes.json')
+    .subscribe((data: someClass[]) => {
+      this.classes = data;
+      this.allClasses = this.classes;
+    });
+  }
+
+  search(value: string): void{
+    this.classes = this.allClasses.filter((val) =>
+    val.name.toLowerCase().includes(value)
+    );
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { SearchFilterPipe } from './search-filter.pipe';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    SearchFilterPipe,
   ],
   imports: [
     BrowserModule,
-    NoopAnimationsModule
+    NoopAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    Ng2SearchPipeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
