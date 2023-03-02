9e1962d71cd2e5689d08e009545b9487ef54d53c
diminikov <diminikov@gmail.com>
2023-03-01 20:04:16 -0500

    Finish Backend Tests

--- gogorm/backend_test.go
+++ gogorm/backend_test.go
@@ -2,14 +2,24 @@ package main
 
 import (
 	"bytes"
+	"gatorrater/database"
+	"gatorrater/models"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
-	//"gatorrater/database"
-
 	"github.com/stretchr/testify/assert"
+	"gorm.io/gorm"
 )
+
+// helper function to check if user is in database
+func checkDBForUser(db *gorm.DB, user *models.User, u string, p string) bool {
+	err := db.Where("username = ?", u).Where("password = ?", p).First(user).Error
+	if err != nil {
+		return false
+	}
+	return true
+}
 
 // testing the connection to the backend
 func TestPingRoute(t *testing.T) {
@@ -23,7 +33,7 @@ func TestPingRoute(t *testing.T) {
 	assert.Equal(t, "\"pong\"", w.Body.String())
 }
 
-func TestGetUser(t *testing.T) {
+func TestMakeUser(t *testing.T) {
 	router := setupRouter()
 
 	var jsonData = []byte(`{
@@ -36,6 +46,52 @@ func TestGetUser(t *testing.T) {
 	req.Header.Add("Content-Type", "application/json")
 	router.ServeHTTP(w, req)
 
-	assert.Equal(t, 200, w.Code)
+	var user models.User
 
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, true, checkDBForUser(database.Db, &user, "morpheus", "leader"))
+}
+
+func TestGetUser(t *testing.T) {
+	router := setupRouter()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/users/morpheus", nil)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+}
+
+func TestEditUser(t *testing.T) {
+	router := setupRouter()
+
+	var jsonData = []byte(`{
+		"Username": "smith",
+		"Password": "agent"
+	}`)
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("PUT", "/users/morpheus", bytes.NewBuffer(jsonData))
+	req.Header.Add("Content-Type", "application/json")
+	router.ServeHTTP(w, req)
+
+	var user models.User
+
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, false, checkDBForUser(database.Db, &user, "morpheus", "leader"))
+	assert.Equal(t, true, checkDBForUser(database.Db, &user, "smith", "agent"))
+}
+
+func TestDeleteUser(t *testing.T) {
+	router := setupRouter()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("DELETE", "/users/smith", nil)
+	router.ServeHTTP(w, req)
+
+	var user models.User
+
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, false, checkDBForUser(database.Db, &user, "morpheus", "leader"))
+	assert.Equal(t, false, checkDBForUser(database.Db, &user, "smith", "agent"))
 }
