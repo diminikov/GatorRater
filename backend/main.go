f8336b652ca14e081526813aa2aac2ecd2fb90da
msawarynski <92892520+msawarynski@users.noreply.github.com>
2023-03-28 16:14:57 -0400

    added working sign up and login

--- backend/main.go
+++ backend/main.go
@@ -15,6 +15,19 @@ func main() {
 
 func setupRouter() *gin.Engine {
 	r := gin.Default()
+	
+	r.Use(func(c *gin.Context) {
+        c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:4200") // Replace with the origin domain of your client-side application
+        c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE")
+        c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+        if c.Request.Method == "OPTIONS" {
+            c.AbortWithStatus(http.StatusNoContent)
+            return
+        }
+        c.Next()
+    })
+
+
 
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, "pong")
