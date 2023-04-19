4c901177894bf46e532ee86b31834169a1c8013d
Yonas Worku <yo.worku@ufl.edu>
2023-04-19 12:45:36 -0400

    About us fixed

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -10,7 +10,7 @@ export class AboutUsComponent {
   persons : AboutUs [] = [
     new AboutUs('Michael Sawarynski', 'Hello, I am from Gulf breeze Fl.', 'Front-End Development','assets/img/MichaelSawarynskiHeadshot.jpeg'),
     new AboutUs('Yonas', 'Hello, I am from Jax.', 'Front-End Development','assets/img/YonasWorku.jpeg'),
-    new AboutUs('Nish', 'Hello, I am from Australia.', 'Back-End Development','assets/img/Nish.jpeg'),
+    new AboutUs('Nish', 'Hello, I am from Australia.', 'Back-End Development','assets/img/nish.jpeg'),
     new AboutUs('Dimitri', 'Hello, I am from Gulf breeze Fl.', 'Front-End Development','assets/img/Dmitri.jpeg' )
 
   ];
